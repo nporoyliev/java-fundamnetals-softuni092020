5e2ff5a28ba9dbdeb533f81b33b34f76a27ac02f
nporoyliev <nporoyliev@gmail.com>
2020-09-19 14:20:47 +0300

    Add class GamingStore.

---
+++ src/com/SoftUniFundamentals/GamingStore.java
@@ -0,0 +1,53 @@
+package com.SoftUniFundamentals;
+
+import java.util.*;
+
+public class GamingStore {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Double> games = new HashMap<>();
+        List<String> boughtGames = new ArrayList<>();
+
+        games.put("OutFall 4", 39.99);
+        games.put("CS:OG", 15.99);
+        games.put("Zplinter Zell", 19.99);
+        games.put("Honored 2", 59.99);
+        games.put("RoverWatch", 29.99);
+        games.put("RoverWatch Origins Edition", 39.99);
+
+
+        double currentBalance = Double.parseDouble(scanner.nextLine());
+        double totalSpent = 0;
+        boolean outOfMoney = false;
+
+
+        String input = scanner.nextLine();
+        while (!"Game Time".equals(input)) {
+
+            if (games.containsKey(input)) {
+                if (games.get(input) <= currentBalance) {
+                    currentBalance -= games.get(input);
+                    totalSpent += games.get(input);
+                    boughtGames.add(input);
+                } else {
+                    System.out.println("Too expensive");
+                }
+            } else {
+                System.out.println("Not found");
+            }
+            if (currentBalance == 0) {
+                outOfMoney = true;
+                break;
+            }
+            input = scanner.nextLine();
+        }
+
+            boughtGames.forEach(e -> System.out.println("Bought " + e));
+            System.out.printf("Total spent: $%.2f. Remaining: $%.2f", totalSpent, currentBalance);
+        if (outOfMoney){
+            System.out.println("Out of money!");
+        }
+    }
+}
