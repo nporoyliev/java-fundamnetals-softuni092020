5e307df34a3640359cdd85af5c14f8dbe4065b36
nporoyliev <nporoyliev@gmail.com>
2020-09-19 11:50:16 +0300

    Added new class RageExpenses.

---
+++ src/com/SoftUniFundamentals/RageExpenses.java
@@ -0,0 +1,31 @@
+package com.SoftUniFundamentals;
+
+import java.util.Scanner;
+
+public class RageExpenses {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberLostGames = Integer.parseInt(scanner.nextLine());
+        double headSetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+
+        double damageCosts = 0;
+
+        for (int i = 1; i <= numberLostGames ; i++) {
+            if (i % 2 ==0){
+                damageCosts += headSetPrice;
+            }
+            if (i % 3 == 0){
+                damageCosts += mousePrice;
+            }
+            if (i % 2 == 0 && i % 3 == 0){
+                damageCosts += keyboardPrice;
+            }
+        }
+    }
+}
